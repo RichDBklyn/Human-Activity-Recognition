7aa390e8dbc2d51d1afb775b8f87ade49df2defb
Rich Delaney <rich_delaney@icloud.com>
2014-07-17 13:37:04 -0400

    R script file with Final Edits

--- run_analysis.R
+++ run_analysis.R
@@ -4,7 +4,7 @@ TestDF<-read.table("X_test.txt")
 #combine training and test data into a single data frame
 CombinedDF<-rbind(TrainDF,TestDF)
 #assign names from original data source to each variable in data frame
-VarLabels<-read.table("features.txt"")
+VarLabels<-read.table("features.txt")
 names(CombinedDF)<-VarLabels$V2
 #create variable with subject IDs for each record and add to combined data frame
 SubjTrain<-read.table("subject_train.txt")
@@ -21,11 +21,15 @@ colnames(CombinedDF)[563]<-"Activity"
 #creates list of names for variables measuring mean or standard deviation
 #also include subject ID and activity variables
 VarNamesSubsetMean<-grep("mean",names(CombinedDF),fixed=TRUE,value=TRUE)
-VarNamesSubsetMean<-grep("meanFreq",VarNamesSubset,fixed=TRUE,value=TRUE,invert=TRUE)
+VarNamesSubsetMean<-grep("meanFreq",VarNamesSubsetMean,fixed=TRUE,value=TRUE,invert=TRUE)
 VarNamesSubsetSD<-grep("std",names(CombinedDF),fixed=TRUE,value=TRUE)
 VarNamesSubset<-c("SubjID","Activity",VarNamesSubsetMean,VarNamesSubsetSD)
 #creates smaller data frame using selected variables
 SubsetDF<-CombinedDF[VarNamesSubset]
+#convert Activity variable to a factor
+FactorLevels<-c(1,2,3,4,5,6)
+FactorLabels<-c("WALKING", "WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING")
+SubsetDF$Activity<-factor(SubsetDF$Activity,levels=FactorLevels,labels=FactorLabels)
 #edit variable names to standard format
 #first four letters are function (ex. "Mean")
 #after underscore, next letter indicates domain [time=t, frequency=f]
@@ -37,10 +41,6 @@ names(SubsetDF)<-gsub("\\()","",names(SubsetDF))
 names(SubsetDF)<-gsub("-","_",names(SubsetDF))
 names(SubsetDF)[3:35]<-paste("Mean_",names(SubsetDF)[3:35],sep="")
 names(SubsetDF)[36:68]<-paste("StDv_",names(SubsetDF)[36:68],sep="")
-#convert Activity variable to a factor
-FactorLevels<-c(1,2,3,4,5,6)
-FactorLabels<-c("WALKING", "WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING")
-SubsetDF$Activity<-factor(SubsetDF$Activity,levels=FactorLevels,labels=FactorLabels)
 #create data frame of summary statistic (mean) of all variables 
 #based on 180 unique combinations of SubjID (n=30) and Activity (n=6)
 SummaryDF<-aggregate(.~SubjID+Activity,data=SubsetDF,mean)
